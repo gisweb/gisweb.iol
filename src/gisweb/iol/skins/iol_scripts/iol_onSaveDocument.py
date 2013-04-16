6b2ee801732f0d44fb27bc9bea1799cd8c41b1a6
Manuele Pesenti <manuele.pesenti@gmail.com>
2013-04-16 12:20:36 +0200

    more actions in iol_onSaveDocument.py

---
+++ src/gisweb/iol/skins/iol_scripts/iol_onSaveDocument.py
@@ -0,0 +1,40 @@
+## Script (Python) "iol_onSaveDocument"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=child_events=False, backToParent=False
+##title=IOL onSaveDocument event common actions
+##
+
+"""
+Standardizzazione dele operazioni da svolgere al salvataggio di una istanza
+child_events: True o False (lancia gli script di gestione dell'uno a molti)
+backToParent: False o True o 'anchor'
+"""
+
+# EVENTI DI REALIZZAZIONE COLLEGAMENTO UNO A MOLTI
+
+if child_events == True:
+    context.event_onSaveChild(backToParent=backToParent)
+
+# Se è stata sospesa, al salvataggio setto un item che abiliti la transizione di integrazione
+if context.wf_getInfoFor('review_state') == 'sospesa':
+    context.setItem('pronta_per_integrazione', 1)
+
+# EVENTI DI WORKFLOW
+
+def doActionIfAny(doc):
+    """
+    Esegue le transizioni del workflow che devono essere eseguite al salvataggio della pratica
+    """
+    from Products.CMFCore.utils import getToolByName
+    pw=getToolByName(doc,'portal_workflow')
+
+    actions = [i['id'] for i in pw.getTransitionsFor(doc) if i['id'] in pw.getInfoFor(doc, 'transition_on_save')]
+
+    if actions:
+        pw.doActionFor(doc,actions[0])
+
+doActionIfAny(context)
