9f0dcb75fc69b8076a76e84c9b1a78bb24b33061
Manuele Pesenti <manuele.pesenti@gmail.com>
2013-04-16 11:22:31 +0200

    added script numerazione.py

---
+++ src/gisweb/iol/skins/iol_scripts/numerazione.py
@@ -0,0 +1,31 @@
+## Script (Python) "numerazione"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=item_name='numero_pratica'
+##title=Gestione della numerazione delle pratiche
+##
+
+"""
+1. verificare l'esistenza di uno script dedicato di numerazione
+2. usare una procedura standard (getMaxOf)
+
+item_name: nome del campo di numerazione. Il campo deve essere indicizzato
+	e di tipo numerico.
+
+questo script sarà usato dalla formula di calcolo del campo.
+"""
+
+nome_script = 'numerazione'
+
+script.naming('tipo_app')
+
+numerazione = context.getParentDatabase().resources[script.naming('tipo_app')].get(nome_script)
+
+if numerazione:
+	return numerazione(item_name)
+else:
+	return script.getMaxOf(item_name) + 1
+	
