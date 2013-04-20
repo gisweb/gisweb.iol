12578711f83d32dc3c64c772ff985920e3d815ee
Silvio Tomatis <silviot@gmail.com>
2013-04-20 12:22:02 +0200

    Jenkins debugging

--- jenkins.sh
+++ jenkins.sh
@@ -4,8 +4,11 @@ if [ "$PLONE_VERSION" == "4.0" ] ; then sed "4 a\    http://good-py.appspot.com/
 if [ "$PLONE_VERSION" == "4.0" ] ; then cat plone_4.0_4.1_versions.txt >> buildout.cfg ; fi
 if [ "$PLONE_VERSION" == "4.1" ] ; then sed "4 a\    http://good-py.appspot.com/release/plone.app.registry/1.0b2?plone=4.1.6" buildout.cfg > buildout.cfg.new && mv buildout.cfg{.new,}  ; fi
 if [ "$PLONE_VERSION" == "4.1" ] ; then cat plone_4.0_4.1_versions.txt >> buildout.cfg ; fi
-virtualenv -p /usr/local/bin/python2.7 env
+virtualenv -p python2.7 env
+pwd
+ls
+ls *
 env/bin/python bootstrap.py
-./bin/buildout
-./bin/test
+bin/buildout
+bin/test
 git checkout buildout.cfg
