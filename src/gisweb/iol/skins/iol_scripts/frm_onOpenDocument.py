3bee169211556ae9e7eaa860866a9c5191d7bfeb
Manuele Pesenti <manuele.pesenti@gmail.com>
2013-04-29 15:27:46 +0200

    openwithform related actions in frm events

---
+++ src/gisweb/iol/skins/iol_scripts/frm_onOpenDocument.py
@@ -0,0 +1,22 @@
+## Script (Python) "iol_onOpenDocument"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=IOL onDeleteDocument event common actions
+##
+
+"""
+Standardizzazione dele operazioni da svolgere alla apertura di una istanza
+"""
+
+# ON OPEN WITH FORM
+
+owf_name = context.REQUEST.get('openwithform')
+
+owf = None if not owf_name else context.getParentDatabase().getForm(owf_name)
+
+if owf and not context.isNewDocument():
+    context.setItem('oForm', context.getItem('Form', ''))
--- src/gisweb/iol/skins/iol_scripts/frm_onSaveDocument.py
+++ src/gisweb/iol/skins/iol_scripts/frm_onSaveDocument.py
@@ -7,21 +7,33 @@
 ##parameters=child_events=False, backToParent=False
 ##title=IOL onSaveDocument event common actions
 ##
-
 """
 Standardizzazione dele operazioni da svolgere al salvataggio di una istanza
 child_events: True o False (lancia gli script di gestione dell'uno a molti)
 backToParent: False o True o 'anchor'
 """
 
+# ON OPEN WITH FORM
+
+if 'oForm' in context.getItems():
+    oFormName = context.getItem('oForm')
+    # previene l'associazione di un form inesistente
+    if context.getParentDatabase().getForm(oFormName):
+        context.setItem('Form', oFormName)
+    context.removeItem('oForm')
+
+
+
 # EVENTI DI REALIZZAZIONE COLLEGAMENTO UNO A MOLTI
 
 if child_events == True:
     context.event_onSaveChild(backToParent=backToParent)
 
+
 # Se è stata sospesa, al salvataggio setto un item che abiliti la transizione di integrazione
 if context.wf_getInfoFor('review_state') == 'sospesa':
     context.setItem('pronta_per_integrazione', 1)
+
 
 # EVENTI DI WORKFLOW
 
