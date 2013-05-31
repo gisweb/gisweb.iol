53aeda3bb84666f9fb6b3a50eeb2d0e7fb56f660
Silvio Tomatis <silviot@gmail.com>
2013-05-31 14:45:20 +0200

    Saner cleanup before jenkins run

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm env -rf
 rm parts/test -rf
+git clean -dff
 if [ "$PLONE_VERSION" == "4.0" ] ; then sed "4 a\    http://good-py.appspot.com/release/plone.app.registry/1.0b2?plone=4.0.9" buildout.cfg > buildout.cfg.new && mv buildout.cfg{.new,}  ; fi
 if [ "$PLONE_VERSION" == "4.0" ] ; then cat plone_4.0_4.1_versions.txt >> buildout.cfg ; fi
 if [ "$PLONE_VERSION" == "4.1" ] ; then sed "4 a\    http://good-py.appspot.com/release/plone.app.registry/1.0b2?plone=4.1.6" buildout.cfg > buildout.cfg.new && mv buildout.cfg{.new,}  ; fi
@@ -13,7 +14,7 @@ env/bin/python bootstrap.py
 # the shebangs of those files don' work; It's probably related to their lengths
 # Explicitly naming the python interpreter is a workaround
 # that will make the buildout and tests run
-env/bin/python bin/develop up -f
+[ -f bin/develop ] && env/bin/python bin/develop up -f
 env/bin/python bin/buildout install test
 # Sleep come time before starting recording to let the fixtures load
 recordmydesktop --delay 70 --no-cursor --no-sound --output=session.ogv &
