23a9a3aa92422ba47aa032e88549484ff4dfb207
mamogmx <marco.carbone@gmx.com>
2013-07-26 16:18:15 +0200

    Added on Open View event

---
+++ src/gisweb/iol/skins/iol_scripts/view_onOpen.py
@@ -0,0 +1,11 @@
+## Script (Python) "view_onOpen"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=IOL on Open View event common actions
+##
+
+db = context.getParentDatabase()
