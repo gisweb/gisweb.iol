3fb048efa1e924833442859822585b5f9ed88b37
Manuele Pesenti <manuele.pesenti@gmail.com>
2013-11-20 11:57:48 +0100

    nothing interesting

--- src/gisweb/iol/__init__.py
+++ src/gisweb/iol/__init__.py
@@ -1,2 +1,6 @@
 from AccessControl import allow_module
 allow_module('gisweb.iol.scripts.frm_beforeCreateDocument')
+
+import permissions
+
+from permissions import IOL_READ_PERMISSION, IOL_EDIT_PERMISSION, IOL_REMOVE_PERMISSION
