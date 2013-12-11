2d4589558edddcbf35e6c59b73d1eea1f07eb5ba
Manuele Pesenti <manuele.pesenti@gmail.com>
2013-12-11 13:48:17 +0100

    some workflow improvements

---
+++ src/gisweb/iol/profiles/default/workflows/iol_workflow/scripts/after_torna_presentata.py
@@ -0,0 +1,21 @@
+## Script (Python) "after_torna_presentata"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=state_change
+##title=
+##
+doc = state_change.object
+
+#Aggiornamento dello stato su plominoDocument
+doc.updateStatus()
+
+if script.run_script(doc, script.id) != False:
+
+    #### OTHER CODE HERE ####
+
+    script.run_script(doc, script.id, suffix='post')
+
+#### SCRIPT ENDS HERE ####
