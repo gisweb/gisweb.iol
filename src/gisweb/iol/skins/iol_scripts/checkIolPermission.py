574f5e6c887dfab5f0d9af21c3945e2f1a9a9745
Manuele Pesenti <manuele.pesenti@gmail.com>
2013-12-16 17:42:06 +0100

    new workflow and actions definition according to new iol permissions

---
+++ src/gisweb/iol/skins/iol_scripts/checkIolPermission.py
@@ -0,0 +1,39 @@
+## Script (Python) "checkIolPermission"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=permission='read', doc=None
+##title=
+##
+
+"""
+Use PlominoUtils.checkUserPermission with shortcuts for custom IOL permissions
+"""
+
+from gisweb.iol import IOL_READ_PERMISSION, IOL_EDIT_PERMISSION, IOL_REMOVE_PERMISSION
+from gisweb.utils import getUserPermissions
+
+db = context.getParentDatabase()
+
+if isinstance(doc, basestring):
+    doc = db.getDocument(doc)
+
+if not doc:
+    doc = context
+
+aliases = dict(read = IOL_READ_PERMISSION,
+    edit = IOL_EDIT_PERMISSION,
+    remove = IOL_REMOVE_PERMISSION)
+
+if permission in aliases:
+    permission = aliases[permission]
+
+def flt(el):
+    rolesInContext = context.portal_membership.getAuthenticatedMember().getRolesInContext(doc)
+    return el['selected'] and (el['name'] in rolesInContext)
+
+return len(filter(flt, doc.rolesOfPermission(permission)))>0
+
+
