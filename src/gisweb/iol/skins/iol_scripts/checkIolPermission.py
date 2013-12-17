901e9e9d7ea65a745ee223e3064f041982bd74f9
Manuele Pesenti <manuele.pesenti@gmail.com>
2013-12-17 11:23:14 +0100

    redefined action formulas. WARNING! This commit needs at least gisweb.utils commit with id 0ee2c4f2

--- src/gisweb/iol/skins/iol_scripts/checkIolPermission.py
+++ src/gisweb/iol/skins/iol_scripts/checkIolPermission.py
@@ -13,7 +13,7 @@ Use PlominoUtils.checkUserPermission with shortcuts for custom IOL permissions
 """
 
 from gisweb.iol import IOL_READ_PERMISSION, IOL_EDIT_PERMISSION, IOL_REMOVE_PERMISSION
-from gisweb.utils import getUserPermissions
+from gisweb.utils import getRolesOfPermission
 
 db = context.getParentDatabase()
 
@@ -34,6 +34,6 @@ def flt(el):
     rolesInContext = context.portal_membership.getAuthenticatedMember().getRolesInContext(doc)
     return el['selected'] and (el['name'] in rolesInContext)
 
-return len(filter(flt, doc.rolesOfPermission(permission)))>0
+return len(filter(flt, getRolesOfPermission(doc, permission)))>0
 
 
