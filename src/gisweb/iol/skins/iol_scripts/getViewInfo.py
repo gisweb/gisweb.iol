6ab9d9fe09b41942f980bc077a8b9c6883fb2be6
Manuele Pesenti <manuele.pesenti@gmail.com>
2013-12-18 17:05:52 +0100

    introduced getViewInfo

---
+++ src/gisweb/iol/skins/iol_scripts/getViewInfo.py
@@ -0,0 +1,21 @@
+## Script (Python) "getViewInfo"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+
+from gisweb.utils import json_dumps
+columns = context.getColumns()
+
+context.REQUEST.RESPONSE.setHeader("Content-type", "application/json")
+print json_dumps([dict(
+    aTargets = [n+1],
+    sTitle = c.Title(),
+    sName = c.id,
+    mData = c.id,
+    bVisible = not getattr(c, 'HiddenColumn', False)) for n,c in enumerate(columns)])
+return printed
