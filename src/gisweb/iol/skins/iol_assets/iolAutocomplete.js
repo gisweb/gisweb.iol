f89f9102fe6b3fd29772197c486c7fcb5f161d15
Roberto <roberto.starnini@gmail.com>
2014-08-06 16:41:03 +0200

    plugin autocompleteform fixed

--- src/gisweb/iol/skins/iol_assets/iolAutocomplete.js
+++ src/gisweb/iol/skins/iol_assets/iolAutocomplete.js
@@ -1,60 +1,63 @@
-//NON SERVE PIU??????????????????????????????????????
-
+// DA SISTEMARE!!!!!!!!!!!!!!!!!!
 (function ($) {
-	
-    $.fn.iolAutocompleteForm = function( options ) {
-        return this.each(function() {
+	$(document).ready(function () {
+        //Autosuggest per l'elenco dei nomi e compilazione campi
+        $("[data-plugin = iolAutocompleteForm]").each(function(){
+          eval("var options = "  + $(this).data('autocompleteformOptions') +" || {}");
+          var baseUrl = $(this).data('baseUrl');
 
-
-            console.log(options)
-           // var baseUrl = $(this).data('baseUrl');
-
-            var formatResult =  function(name){ return" "+name.fisica_cognome+" "+name.fisica_nome+" cf: "+name.fisica_cf+" nato a "+name.fisica_comune_nato+" il "+name.fisica_data_nato+" " };
-            var formatSelection = function(name){return name.fisica_cognome+ " "+name.fisica_nome+" cf: "+name.fisica_cf };
-            var data = function(term,page){return {"forms":"base_sub_fisica","distinct":"fisica_cf","fisica_cf":term,"page":page,"size":10}};
-            $element = $(this);
-            console.log($element)
-            $element.select2({
-                placeholder: "Digitare il codice fiscale della persona da ricercare",
-                allowClear: true,
-                minimumInputLength: 0,
-                id: $element.attr('id'),
-                formatResult: formatResult,
-                formatSelection: formatSelection,
-                ajax: {
-                    url: "fetch_data",
-                    dataType: 'json',
-                    quietMillis: 100,
-                    data: data,
-                    results: function (data, page) {
-                        var more = (page * 10) < data.total; // whether or not there are more results available
-             
-                        // notice we return the value of more so Select2 knows if more results can be loaded
-                        return {results: data.names, more: more};
-                    }
-                },
-                escapeMarkup: function (m) { return m; } // we do not want to escape markup since we are displaying html in results
-            }).on("change", function(e) { 
-                var childs=e.added;
-                if(e.removed){
-                        if(!confirm('Attenzione i dati già inseriti verranno rimossi, continuo?')) return;
-                        childs=e.removed; 
-                } 
-                $.each(childs, function(key,value) {
-                    var el;
-                    if($('[name='+key+']').length>0){
-                        el = $('[name='+key+']').eq(0);
-                        if(e.added) 
-                            setControlValue(el,value);                 
-                        else if(e.removed)
-                            setControlValue(el,'');
-                        el.trigger('change');
-                    }
+          $(this).select2({
+            placeholder: options.placeolder,
+            allowClear: options.allowClear,
+            minimumInputLength: options.minimumInputLength,
+            id: options.id,
+            formatResult: options.formatResult,
+            formatSelection: options.formatSelection,
+            ajax: {
+                url: options.url,
+                dataType: 'json',
+                quietMillis: 100,
+                data: options.data,
+                results: function (data, page) {
+                    var more = (page * 10) < data.total; // whether or not there are more results available
+         
+                    // notice we return the value of more so Select2 knows if more results can be loaded
+                    return {results: data.names, more: more};
+                }
+              },
+              escapeMarkup: function (m) { return m; } // we do not want to escape markup since we are displaying html in results
+          }).on("change", function(e) { 
+                     var childs=e.added;
+                     if(e.removed){
+                            if(!confirm('Attenzione i dati già inseriti verranno rimossi, continuo?')) return;
+                            childs=e.removed; 
+                     } 
+                     $.each(childs, function(key,value) {
+                          var el;
+                          if($('[name='+key+']').length>0){
+                               el = $('[name='+key+']').eq(0);
+                               if(e.added) 
+                                   setControlValue(el,value);                 
+                               else if(e.removed)
+                                    setControlValue(el,'');
+                                el.trigger('change');
+            
+                           }
+                     });
+                     //console.log("change "+JSON.stringify({val:e.val, added:e.added, removed:e.removed})); 
                 });
-                 //console.log("change "+JSON.stringify({val:e.val, added:e.added, removed:e.removed})); 
-            });
-
         });
-    };
 
+        //ATTENZIONE SISTEMARE IN SERIALDOC LA RESTITUZIONE DEI VALORI AL POSTO DELLE LABEL, PER ORA DOVE SERVE USO VALORE=LABEL
+        function setControlValue(el,value){
+           if(el.is(':checkbox')||el.is(':radio')) {
+              $("#"+el.attr("name")+'-'+value).attr('checked', true);
+              //console.log("#"+el.attr("name")+'-'+value)
+            }
+           else{
+              el.val(value)
+           }
+        }
+
+    });
 })(jQuery);
