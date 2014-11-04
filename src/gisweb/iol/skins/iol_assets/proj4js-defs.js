172a2535f24fbd2eb12d19b3bee730e074e07e34
Roberto <roberto.starnini@gmail.com>
2014-11-04 11:35:22 +0100

    minor improvements

--- src/gisweb/iol/skins/iol_assets/proj4js-defs.js
+++ src/gisweb/iol/skins/iol_assets/proj4js-defs.js
@@ -1,4 +1,5 @@
 Proj4js.defs["GOOGLE"]="+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs";
 Proj4js.defs["EPSG:900913"]=Proj4js.defs["GOOGLE"];
+Proj4js.defs["EPSG:3857"]=Proj4js.defs["GOOGLE"];
 Proj4js.defs["EPSG:26591"] = "+title= Monte Mario (Rome) / Italy zone 1 EPSG:26591 +proj=tmerc +lat_0=0 +lon_0=-3.45233333333333 +from_greenwich=12.45233333333333 +k=0.999600 +x_0=1500000 +y_0=0 +a=6378388.0, +b=6356911.94612795 +units=m";
 Proj4js.defs["EPSG:3003"] = "+title= Monte Mario (Rome) / Italy zone 1 EPSG:3003 +proj=tmerc +lat_0=0 +lon_0=9 +k=0.9996 +x_0=1500000 +y_0=0 +ellps=intl +units=m +no_defs +towgs84=-104.1,-49.1,-9.9,0.971,-2.917,0.714,-11.68";
