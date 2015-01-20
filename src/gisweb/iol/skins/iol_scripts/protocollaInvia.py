dcf82cef1f807246ddf3d582254ceefceba8d440
Marco Carbone <marco.carbone@gmx.com>
2015-01-20 16:27:18 +0100

    Update protocollaInvia.py

--- src/gisweb/iol/skins/iol_scripts/protocollaInvia.py
+++ src/gisweb/iol/skins/iol_scripts/protocollaInvia.py
@@ -7,6 +7,7 @@
 ##parameters=
 ##title=
 ##
+from iol.utils.interfaces import IolDocument
 
 if context.portal_type!='PlominoDocument':
     context.REQUEST.RESPONSE.redirect(context.absolute_url())
@@ -19,7 +20,7 @@ next_tr = 'protocolla'
 if next_tr in tr_ids:
     wf.doActionFor(context, next_tr)
 
-context.updateStatus()
+IolDocument(context).updateStatus()
 db = context.getParentDatabase()
 urlAction='%s/content_status_modify?workflow_action=invia_domanda' %(context.absolute_url())
 context.REQUEST.RESPONSE.redirect(urlAction)
