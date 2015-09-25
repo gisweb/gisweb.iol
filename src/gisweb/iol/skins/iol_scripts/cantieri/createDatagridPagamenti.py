292ba0e6f415fb9fc23c84d9fcd3acc63bb8d272
vitolab <istanze@sit.spezianet.it>
2015-09-25 15:30:00 +0200

    bug fix pagamenti avvio cantieri

--- src/gisweb/iol/skins/iol_scripts/cantieri/createDatagridPagamenti.py
+++ src/gisweb/iol/skins/iol_scripts/cantieri/createDatagridPagamenti.py
@@ -137,6 +137,8 @@ if not doc.getItem(field_dg):
     return createDatagrid(diz_pagamenti,stato_pagamento='non pagato')
         
 elif doc.getItem(field_dg):
+    codici_gruppo = [i['gruppo_sub_pagamento'] for i in doc.translateListToDiz(doc.getId(),form='sub_elenco_pagamenti',field='elenco_pagamenti')]
+   
     
     if doc.wf_getInfoFor('review_state') == 'avvio': 
         if len(allegato_bolli) > 0:
@@ -144,6 +146,9 @@ elif doc.getItem(field_dg):
             return updateDatagrid(diz_pagamenti,diz_code_pagamenti={},stato_pagamento='pagamento effettuato',dg_exist=dg_esistente,allegato=True,codice_allegato=codice_allegato)
         elif  len(allegato_bolli_utente) > 0:
             return updateDatagrid(diz_pagamenti,diz_code_pagamenti={},stato_pagamento='in attesa di verifica',dg_exist=dg_esistente,allegato=True,codice_allegato=codice_allegato)       
+        elif doc.getItem('esito_pagamento','KO')=='OK' and doc.getItem('codTrans_pagamento','')!='':
+	    if doc.getItem('codTrans_pagamento').split('-')[-1] in codici_gruppo:                
+                return updateDatagrid(diz_pagamenti,diz_code_pagamenti={},stato_pagamento='pagamento effettuato',dg_exist=dg_esistente,allegato=True,codice_allegato=codice_allegato)         
         else:
             
             return updateDatagrid(diz_pagamenti,diz_code_pagamenti={},stato_pagamento='non pagato',dg_exist=dg_esistente,allegato=True,codice_allegato=codice_allegato)
