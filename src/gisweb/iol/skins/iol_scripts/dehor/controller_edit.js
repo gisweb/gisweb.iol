f253df54b20c25018593be6c210628f24be3ff89
vitolab <istanze@sit.spezianet.it>
2015-09-25 15:33:33 +0200

    update controller js dehor

--- src/gisweb/iol/skins/iol_scripts/dehor/controller_edit.js
+++ src/gisweb/iol/skins/iol_scripts/dehor/controller_edit.js
@@ -69,6 +69,42 @@ $(document).on('datatable_complete',function(e){
 }); */
 
 
+var url_popup_origin = $("#elenco_rate_pagamenti_addrow").attr('href');
+$("#elenco_rate_pagamenti_addrow").bind('click',function(){
+   
+   valore_dg = 'elenco_rate_pagamenti';
+    
+    var ordine_gruppo = [];
+    var ordine_codice = [];
+    var url_popup = '';
+    var dgData = $("#"+ valore_dg +"_datagrid").dataTable().fnGetData() 
+    if(dgData.length>0){
+      for(i=0; i<dgData.length; i++){   
+        var gruppo = dgData[i][3];
+        var codice = dgData[i][0];
+        if ($(gruppo).html()!= null){
+          gruppo = $(gruppo).html().trim()
+        };
+        if ($(codice).html()!= null){
+          codice = $(codice).html().trim()
+        };        
+        ordine_gruppo.push(gruppo);
+        ordine_codice.push(codice);      
+      };
+      
+      last_gruppo = ordine_gruppo.sort().slice(-1);
+      last_codice = ordine_codice.sort().slice(-1);
+      new_group = '00' + String(parseInt(last_gruppo[0])+1);
+      new_codice = '0' + String(parseInt(last_codice[0])+1);
+      url_popup = url_popup_origin;
+      url_popup += "&gruppo_sub_pagamento=" + new_group;
+      url_popup += "&codice_sub_pagamento=" + new_codice;
+      $("#elenco_rate_pagamenti_addrow").attr('href',url_popup);
+
+         
+    };
+  
+});
 
 
 
