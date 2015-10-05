83a33e87a1f98f3ae03a14dd1ef69eb455b216be
vitolab <istanze@sit.spezianet.it>
2015-10-05 18:23:32 +0200

    bug fix verificaPagamento cantieri

--- src/gisweb/iol/skins/iol_scripts/cantieri/verificaPagamento.py
+++ src/gisweb/iol/skins/iol_scripts/cantieri/verificaPagamento.py
@@ -14,8 +14,8 @@ from Products.CMFCore.utils import getToolByName
 rurl=doc.absolute_url()
 
 def settaElencoTransazioni():
-    trans = doc.getItem('codTrans_pagamento','')
-    if trans!='':
+    trans = doc.REQUEST.get('codTrans_pagamento','') or doc.getItem('codTrans_pagamento','')
+    if trans!='' or trans!=None:
         elenco = doc.getItem('elencoTransazioniPagamenti',[])
         if elenco == [] or elenco==None:         
             elenco = [trans]
@@ -27,6 +27,7 @@ def settaElencoTransazioni():
 wf = getToolByName(doc, 'portal_workflow')
 transitions_avb =  [act['id'] for act in doc.wf_transitionsInfo(wf_id='iol_cantieri_wf', args=['description'])]
 if doc.wf_getInfoFor('review_state') == 'pagamenti_avvio' and doc.getItem('esito_pagamento')=='OK':
+    
     settaElencoTransazioni()
     wf.doActionFor(doc, 'effettua_pagamento_bolli')
 elif 'effettua_pagamento' in transitions_avb and doc.getItem('esito_pagamento')=='OK':
